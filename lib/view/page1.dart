e004e1f928e2f70983c630b1337ec91cfa23b2fd
menes0614 <enes.ergul@bil.omu.edu.tr>
2022-02-04 22:33:29 +0300

    proje firebase e bağlandı

---
+++ lib/view/page1.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ana Sayfa"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              "Hayırlı ve uğurlu olsun",
+              style: TextStyle(fontSize: 40),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
